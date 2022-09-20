c6bd4a15da927176fc0783cf93bde0d3d2976f38
JadenFicklin <jadendficklin@gmail.com>
2022-09-20 13:04:52 -0600

    added some file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import Insta from "./components/Insta";
 import Landing from "./components/Landing";
+import Nav from "./components/Nav";
+import Options from "./components/Options";
 
 function App() {
   return (
     <>
+      <Nav />
       <Landing />
+      <Banner />
+      <Options />
+      <Insta />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Insta.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Insta() {
+  return <div>Insta</div>;
+}
+
+export default Insta;
