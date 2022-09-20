e88ba0aafda3f1bf98b6fd968949c0f1c7e00ad0
JadenFicklin <jadendficklin@gmail.com>
2022-09-20 17:00:01 -0600

    added banner info

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,35 @@
 import React from "react";
 import "../styles/Banner.css";
+import { BsFacebook } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { RiInstagramFill } from "react-icons/ri";
+import { BsLinkedin } from "react-icons/bs";
 
 function Banner() {
   return (
     <>
       <div className="banner-outer">
-        <div className="banner-icons-one"></div>
-        <div className="banner-text-one"></div>
-        <div className="banner-text-two"></div>
-        <div className="banner-text-three"></div>
-        <div className="banner-icons-two"></div>
+        <div className="banner-text-one">I SPECIALIZE IN</div>
+        <div className="banner-text-two">
+          <em>BEAUTIFUL</em>
+        </div>
+        <div className="banner-text-three">
+          COSMETIC TATTOOING IN CHARLOTTE, NC
+        </div>
+        <div className="banner-icons">
+          <div className="icon">
+            <BsFacebook className="icon-import" />
+          </div>
+          <div className="icon">
+            <BsTwitter className="icon-import" />
+          </div>
+          <div className="icon">
+            <RiInstagramFill className="icon-import" />
+          </div>
+          <div className="icon">
+            <BsLinkedin className="icon-import" />
+          </div>
+        </div>
       </div>
     </>
   );
