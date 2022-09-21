4c1f1541e19c23d123c7adde354dac81fd5a6d68
JadenFicklin <jadendficklin@gmail.com>
2022-09-20 22:18:40 -0600

    3 of the boxes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "../styles/Footer.css";
+import { BsFacebook } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { RiInstagramFill } from "react-icons/ri";
+import { BsLinkedin } from "react-icons/bs";
 
 function Footer() {
   return (
     <>
-      <div className="footer-outer"></div>
+      <div className="footer-outer">
+        <div className="footer-icons">
+          <div className="footer-icon">
+            <BsFacebook className="footer-icon-import" />
+          </div>
+          <div className="footer-icon">
+            <BsTwitter className="footer-icon-import" />
+          </div>
+          <div className="footer-icon">
+            <RiInstagramFill className="footer-icon-import" />
+          </div>
+          <div className="footer-icon">
+            <BsLinkedin className="footer-icon-import" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Options.css";
 import Insta from "./Insta";
 
 function Options() {
+  let mystring = ">";
+  const [girl, setGirl] = useState(true);
+
   return (
     <>
       <div className="options-outer">
         <div className="options-top">
           <div className="boxes-top">
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
+            <div className="box">
+              <div className="lip-tattoo-top"></div>
+              <div className="lip-tattoo-bottom">
+                <div className="lip-tattoo-bottom-inner">
+                  <div className="lip-tattoo-bottom-header h">Lip Tattoo</div>
+                  <div className="lip-tattoo-bottom-sub-header s">
+                    Enhance the shape and the color of your lips with the latest
+                    lip blush tattoo
+                  </div>
+                  <div className="lip-tattoo-bottom-button b">
+                    Read More {mystring}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="box">
+              <div
+                className="box-decoration-inner"
+                onMouseEnter={() => setGirl(false)}
+                onMouseLeave={() => setGirl(true)}
+              >
+                <div className="girl"></div>
+                <div className={girl ? "circle" : "circle-expanded"}></div>
+              </div>
+            </div>
+            <div className="box">
+              <div className="quote-box">
+                <div className="quote">
+                  The best decision I had made. Absolutely natural amazing long
+                  lasting result.
+                  <div className="quote-credit">jojo baggins</div>
+                </div>
+              </div>
+            </div>
             <div className="box"></div>
           </div>
           <div className="boxes-bottom">
