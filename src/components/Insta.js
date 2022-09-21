0dab5e6a9d997366d4f9635f86714dcd8ad3a71c
JadenFicklin <jadendficklin@gmail.com>
2022-09-20 18:00:16 -0600

    added insta banner

--- src/components/Insta.js
+++ src/components/Insta.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/Insta.css";
+import { GrInstagram } from "react-icons/gr";
 
 function Insta() {
-  return <div>Insta</div>;
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <>
+      <div className="insta-outer">
+        Beauty by Kerrie<br></br>
+        301 Posts <br></br>BROWS LIPS EYES <br></br>• MOST natural cosmetic
+        tattoo in Charlotte, NC <br></br>• Over 6 years of experience <br></br>
+        <br></br>
+        DM for any inquiries and booking
+      </div>
+      <div
+        className="follow-button"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <GrInstagram className="insta-icon" />
+        Follow
+        {isHovered ? (
+          <div className="follow-message">
+            Clicking this will open Instagram in a new tab
+            <div className="follow-arrow"></div>
+          </div>
+        ) : null}
+      </div>
+    </>
+  );
 }
 
 export default Insta;
--- src/components/Options.js
+++ src/components/Options.js
@@ -6,7 +6,22 @@ function Options() {
   return (
     <>
       <div className="options-outer">
-        <Insta />
+        <div className="options-top">
+          <div className="boxes-top">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+          </div>
+          <div className="boxes-bottom">
+            <div className="box-wide"></div>
+            <div className="box bot"></div>
+            <div className="box bot"></div>
+          </div>
+        </div>
+        <div className="options-bottom">
+          <Insta />
+        </div>
       </div>
     </>
   );
